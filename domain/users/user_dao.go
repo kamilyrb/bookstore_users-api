d882fb2e4f0a3e517242f501ddb6dfa626839d60
Kamil Yarbaşı <kamilyrb@gmail.com>
2021-05-16 21:42:53 +0300

    feature/working-with-date - add date_utils fro using dates from common functions - add DateCreated field when user creation

--- domain/users/user_dao.go
+++ domain/users/user_dao.go
@@ -2,6 +2,7 @@ package users
 
 import (
 	"fmt"
+	"github.com/kamilyrb/bookstore_users-api/utils/date_utils"
 	"github.com/kamilyrb/bookstore_users-api/utils/errors"
 )
 
@@ -30,6 +31,7 @@ func (user *User) Save() *errors.RestErr {
 		}
 		return errors.NewBadRequestError(fmt.Sprintf("user %d already exists", user.Id))
 	}
+	user.DateCreated = date_utils.GetNowString()
 	usersDB[user.Id] = user
 	return nil
 }
