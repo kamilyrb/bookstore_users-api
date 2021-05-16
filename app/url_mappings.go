6fb5f0adc5007d1daba3cdfdafecf7069d42c67f
Kamil Yarbaşı <kamilyrb@gmail.com>
2021-05-16 17:57:36 +0300

    initial-structure: - create structure of project (mvc pattern) - add gin framework for routing endpoints - add users controller and urls basically - add ping controller

---
+++ app/url_mappings.go
@@ -0,0 +1,14 @@
+package app
+
+import (
+	"github.com/kamilyrb/bookstore_users-api/controllers/ping"
+	"github.com/kamilyrb/bookstore_users-api/controllers/users"
+)
+
+func mapUrls() {
+	router.GET("/ping", ping.Ping)
+
+	router.GET("/users/:user_id", users.GetUser)
+	router.GET("/users/search", users.SearchUser)
+	router.POST("/users", users.CreateUser)
+}
