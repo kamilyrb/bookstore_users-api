3610c9ece91aca6011817cf3eb0755c67e24df0b
Kamil Yarbaşı <kamilyrb@gmail.com>
2021-05-16 20:08:00 +0300

    feature/error-handling - add utils/rest_errors for using common error pattern on returning errors - extend user controller and service

--- controllers/users/users_controller.go
+++ controllers/users/users_controller.go
@@ -1,12 +1,28 @@
 package users
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/kamilyrb/bookstore_users-api/domain/users"
+	"github.com/kamilyrb/bookstore_users-api/services"
+	"github.com/kamilyrb/bookstore_users-api/utils/errors"
 	"net/http"
 )
 
 func CreateUser(c *gin.Context) {
-	c.String(http.StatusNotImplemented, "implement me")
+	var user users.User
+	if err := c.ShouldBindJSON(&user); err != nil {
+		fmt.Println(err)
+		restErr := errors.NewBadRequestError("invalid json body")
+		c.JSON(restErr.Status, restErr)
+		return
+	}
+	result, saveErr := services.CreateUser(user)
+	if saveErr != nil {
+		c.JSON(saveErr.Status, saveErr)
+	}
+	fmt.Println(user)
+	c.JSON(http.StatusCreated, result)
 }
 
 func GetUser(c *gin.Context) {
