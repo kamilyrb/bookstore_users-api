f0fb5a31b75ebc81faa0dfe735dd4c7cb0de6cb8
Kamil Yarbaşı <kamilyrb@gmail.com>
2021-05-16 21:13:16 +0300

    feature/data-persistence - expand rest errors - add user dao - add validate method to user dto - implement createuser and getuser service functions

--- controllers/users/users_controller.go
+++ controllers/users/users_controller.go
@@ -1,18 +1,17 @@
 package users
 
 import (
-	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/kamilyrb/bookstore_users-api/domain/users"
 	"github.com/kamilyrb/bookstore_users-api/services"
 	"github.com/kamilyrb/bookstore_users-api/utils/errors"
 	"net/http"
+	"strconv"
 )
 
 func CreateUser(c *gin.Context) {
 	var user users.User
 	if err := c.ShouldBindJSON(&user); err != nil {
-		fmt.Println(err)
 		restErr := errors.NewBadRequestError("invalid json body")
 		c.JSON(restErr.Status, restErr)
 		return
@@ -21,12 +20,22 @@ func CreateUser(c *gin.Context) {
 	if saveErr != nil {
 		c.JSON(saveErr.Status, saveErr)
 	}
-	fmt.Println(user)
 	c.JSON(http.StatusCreated, result)
 }
 
 func GetUser(c *gin.Context) {
-	c.String(http.StatusNotImplemented, "implement me")
+	userId, userErr := strconv.ParseInt(c.Param("user_id"), 10, 64)
+	if userErr != nil {
+		err := errors.NewBadRequestError("user id should be a number")
+		c.JSON(err.Status, err)
+		return
+	}
+	user, getErr := services.GetUser(userId)
+	if getErr != nil {
+		c.JSON(getErr.Status, getErr)
+		return
+	}
+	c.JSON(http.StatusOK, user)
 
 }
 
