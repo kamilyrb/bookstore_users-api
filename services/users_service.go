f18ef78a5939b86eba77281235b95daa86fd209b
Kamil Yarbaşı <kamilyrb@gmail.com>
2021-05-30 23:50:59 +0300

    feature/dependency-editing - remove utils/errors, use util repo instead of it

--- services/users_service.go
+++ services/users_service.go
@@ -4,7 +4,7 @@ import (
 	"github.com/kamilyrb/bookstore_users-api/domain/users"
 	"github.com/kamilyrb/bookstore_users-api/utils/crypto_utils"
 	"github.com/kamilyrb/bookstore_users-api/utils/date_utils"
-	"github.com/kamilyrb/bookstore_users-api/utils/errors"
+	"github.com/kamilyrb/bookstore_utils-go/rest_errors"
 )
 
 var (
@@ -15,15 +15,15 @@ type usersService struct {
 }
 
 type usersServiceInterface interface {
-	GetUser(int64) (*users.User, *errors.RestErr)
-	CreateUser(users.User) (*users.User, *errors.RestErr)
-	UpdateUser(bool, users.User) (*users.User, *errors.RestErr)
-	DeleteUser(int64) *errors.RestErr
-	SearchUser(string) (users.Users, *errors.RestErr)
-	LoginUser(users.LoginRequest) (*users.User, *errors.RestErr)
+	GetUser(int64) (*users.User, *rest_errors.RestErr)
+	CreateUser(users.User) (*users.User, *rest_errors.RestErr)
+	UpdateUser(bool, users.User) (*users.User, *rest_errors.RestErr)
+	DeleteUser(int64) *rest_errors.RestErr
+	SearchUser(string) (users.Users, *rest_errors.RestErr)
+	LoginUser(users.LoginRequest) (*users.User, *rest_errors.RestErr)
 }
 
-func (s *usersService) GetUser(userId int64) (*users.User, *errors.RestErr) {
+func (s *usersService) GetUser(userId int64) (*users.User, *rest_errors.RestErr) {
 	result := &users.User{Id: userId}
 	if err := result.Get(); err != nil {
 		return nil, err
@@ -31,7 +31,7 @@ func (s *usersService) GetUser(userId int64) (*users.User, *errors.RestErr) {
 	return result, nil
 }
 
-func (s *usersService) CreateUser(user users.User) (*users.User, *errors.RestErr) {
+func (s *usersService) CreateUser(user users.User) (*users.User, *rest_errors.RestErr) {
 	if err := user.Validate(); err != nil {
 		return nil, err
 	}
@@ -44,7 +44,7 @@ func (s *usersService) CreateUser(user users.User) (*users.User, *errors.RestErr
 	return &user, nil
 }
 
-func (s *usersService) UpdateUser(isPartial bool, user users.User) (*users.User, *errors.RestErr) {
+func (s *usersService) UpdateUser(isPartial bool, user users.User) (*users.User, *rest_errors.RestErr) {
 	current, err := s.GetUser(user.Id)
 	if err != nil {
 		return nil, err
@@ -72,17 +72,17 @@ func (s *usersService) UpdateUser(isPartial bool, user users.User) (*users.User,
 	return current, nil
 }
 
-func (s *usersService) DeleteUser(userId int64) *errors.RestErr {
+func (s *usersService) DeleteUser(userId int64) *rest_errors.RestErr {
 	user := &users.User{Id: userId}
 	return user.Delete()
 }
 
-func (s *usersService) SearchUser(status string) (users.Users, *errors.RestErr) {
+func (s *usersService) SearchUser(status string) (users.Users, *rest_errors.RestErr) {
 	dao := &users.User{}
 	return dao.FindByStatus(status)
 }
 
-func (s *usersService) LoginUser(request users.LoginRequest) (*users.User, *errors.RestErr) {
+func (s *usersService) LoginUser(request users.LoginRequest) (*users.User, *rest_errors.RestErr) {
 	dao := &users.User{
 		Email:    request.Email,
 		Password: crypto_utils.GetMd5(request.Password),
--- utils/mysql_utils/mysql_utils.go
+++ utils/mysql_utils/mysql_utils.go
@@ -2,7 +2,7 @@ package mysql_utils
 
 import (
 	"github.com/go-sql-driver/mysql"
-	"github.com/kamilyrb/bookstore_users-api/utils/errors"
+	"github.com/kamilyrb/bookstore_utils-go/rest_errors"
 	"strings"
 )
 
@@ -10,17 +10,17 @@ const (
 	ErrorNoRows = "no rows in result set"
 )
 
-func ParseError(err error) *errors.RestErr {
+func ParseError(err error) *rest_errors.RestErr {
 	sqlErr, ok := err.(*mysql.MySQLError)
 	if !ok {
 		if strings.Contains(err.Error(), ErrorNoRows) {
-			return errors.NewNotFoundError("no record matching given id")
+			return rest_errors.NewNotFoundError("no record matching given id")
 		}
-		return errors.NewInternalServerError("error parsing database response")
+		return rest_errors.NewInternalServerError("error parsing database response", err)
 	}
 	switch sqlErr.Number {
 	case 1062:
-		return errors.NewBadRequestError("invalid data")
+		return rest_errors.NewBadRequestError("invalid data")
 	}
-	return errors.NewInternalServerError("error processing request")
+	return rest_errors.NewInternalServerError("error processing request", err)
 }
